e097310a2f556c6e7cabf6d05b29a94d040f5848
Hasan Kacar <hasan.kacar@kodpit.com>
2025-08-31 15:47:19 +0300

    Auth bundle structure added without bundle structure with lexik and security side

---
+++ src/Service/Authenticator/AuthToken.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Service\Authenticator;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Security\Authenticator\Token\JWTPostAuthenticationToken;
+
+class AuthToken extends JWTPostAuthenticationToken
+{
+
+}
